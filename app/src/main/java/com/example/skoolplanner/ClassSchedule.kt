adf05126d8945dee5a8093e938aed10764cad344
Cruz462 <Cruz462@csu.fullerton.edu>
2021-09-22 22:30:23 -0700

    Added a couple lines to the draft Class Schedule class

--- app/src/main/java/com/example/skoolplanner/ClassSchedule.kt
+++ app/src/main/java/com/example/skoolplanner/ClassSchedule.kt
@@ -1,5 +1,17 @@
 package com.example.skoolplanner
 
 class ClassSchedule {
-    // Add code here
+
+    var class_name : String = ""
+    var class_day : String = ""
+    var class_time : Int = 0
+
+
+    fun getClassInfo() {
+        // TODO: This function gets the inputs of the user and stores them accordingly
+    }
+
+    fun display() {
+        // TODO: This function displays the classes the user has inputted and their time and day
+    }
 }
