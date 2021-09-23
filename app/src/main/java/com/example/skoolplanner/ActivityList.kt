3575cfb40620e100211ddaabf6e0195650851d89
jackloague1 <jloague@csu.fullerton.edu>
2021-09-23 01:54:02 -0700

    Created a class for the activity list feature.

---
+++ app/src/main/java/com/example/skoolplanner/ActivityList.kt
@@ -0,0 +1,14 @@
+package com.example.skoolplanner
+
+// A class for listing all assignments and projects
+class ActivityList {
+
+    fun displayList() {
+        // CODE HERE
+    }
+
+    fun sortList() {
+        // CODE HERE
+    }
+
+}
