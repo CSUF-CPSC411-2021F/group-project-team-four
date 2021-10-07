796c947291fb29eb5c118740af509b9e13cf3834
jackloague1 <jloague@csu.fullerton.edu>
2021-10-07 03:50:12 -0700

    I created a basic UI layout for the activity list feature.

--- app/src/main/java/com/example/skoolplanner/ActivityList.kt
+++ app/src/main/java/com/example/skoolplanner/ActivityList.kt
@@ -1,14 +1,53 @@
 package com.example.skoolplanner
 
-// A class for listing all assignments and projects
-class ActivityList {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-    fun displayList() {
-        // CODE HERE
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ActivityList.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ActivityList : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
     }
 
-    fun sortList() {
-        // CODE HERE
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_list, container, false)
     }
 
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ActivityList.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ActivityList().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
