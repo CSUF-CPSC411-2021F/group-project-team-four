78c1180a2f169e1c2407dfdbbf2e8b294315bdab
preet <preetdesai025@csu.fullerton.edu>
2021-10-07 15:21:32 -0700

    Added Methods for Creating, Modifying, and Deleting Exam Objects. Added constraint layout for creating exam. Added Date Picker and Time Picker material elements to layout.

--- app/src/main/java/com/example/skoolplanner/Exam.kt
+++ app/src/main/java/com/example/skoolplanner/Exam.kt
@@ -1,14 +1,12 @@
 package com.example.skoolplanner
 
-import java.sql.Time
-import java.util.*
-
 // Exam class creates exam and quiz objects
 // Exam view will display all upcoming quizzes and tests
 // Users will receive periodical notifications to study
 
-class Exam(name: String, date: String, time: String, description: String) {
+class Exam(className: String, name: String, date: String, time: String, description: String) {
 
+    val className = className
     val examName = name
     val examDate = date
     val examTime = time
@@ -20,9 +18,28 @@ class Exam(name: String, date: String, time: String, description: String) {
 
 }
 
+fun createExam(
+    className: String,
+    name: String,
+    date: String,
+    time: String,
+    description: String
+): Exam {
+    return Exam(className, name, date, time, description)
+}
+
+fun deleteExam() {
+
+}
+
+fun modifyExam() {
+
+}
+
 fun main() {
     // Store all upcoming exams and quizzes in a list.
     val examList = mutableListOf<Exam>()
-    val midterm1 = Exam("CPSC 411 Midterm", "09/30/2021", "4:00PM", "Review Codelabs 1-5")
+    val midterm1 = createExam("CPSC 411", "Midterm", "09/30/2021", "4:00PM", "Review Codelabs 1-5")
     examList.add(midterm1)
+    print("The ${examList[0].className} ${examList[0].examName} will take place on ${examList[0].examDate} at ${examList[0].examTime}")
 }
--- app/src/main/java/com/example/skoolplanner/MainActivity.kt
+++ app/src/main/java/com/example/skoolplanner/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.skoolplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.create_exam)
+
+        val timePicker: TextView = findViewById(R.id.select_time)
+        timePicker.setOnClickListener {
+            openTimePicker()
+        }
+
+        val datePicker: TextView = findViewById(R.id.select_date)
+        datePicker.setOnClickListener {
+            openDatePicker()
+        }
     }
+
+    private fun openDatePicker() {
+        val picker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select Date")
+            .build()
+        picker.show(supportFragmentManager, "tag")
+    }
+
+    private fun openTimePicker() {
+        val clockFormat = TimeFormat.CLOCK_12H
+
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(clockFormat)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Pick date and time")
+            .build()
+        picker.show(supportFragmentManager, "Tag")
+    }
+
+
 }
