759762f5d4430c5fa6dfda3fbf4328bccee095d1
jackloague1 <jloague@csu.fullerton.edu>
2021-10-21 10:37:21 -0700

    I created a new fragment named "CreateActivity." I also added navigation between the ActivityList and CreateActivity fragments.

--- app/src/main/java/com/example/skoolplanner/CreateActivity.kt
+++ app/src/main/java/com/example/skoolplanner/CreateActivity.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Spinner
 import androidx.navigation.findNavController
-import com.example.skoolplanner.databinding.ActivityListBinding
+import com.example.skoolplanner.databinding.CreateActivityBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,12 +15,20 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ActivityList.newInstance] factory method to
+ * Use the [CreateActivity.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ActivityList : Fragment() {
+class CreateActivity : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
@@ -29,21 +36,16 @@ class ActivityList : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = ActivityListBinding.inflate(layoutInflater)
+        val binding = CreateActivityBinding.inflate(layoutInflater)
 
-        // Navigates from the ActivityList view to the CreateActivity view when "Add Activity"
-        // button is clicked on
-        binding.addActivityButton.setOnClickListener { view: View ->
+        // Moves from the CreateActivity view to the ActivityList view when the "Add" button is
+        // clicked on
+        binding.addButton.setOnClickListener{ view: View ->
             view.findNavController()
-                .navigate(R.id.action_activityList_to_createActivity)
+                .navigate(R.id.action_createActivity_to_activityList)
         }
 
-        // Return a link to the layout root
         return binding.root
-    }
-
-    fun sortList() {
-
     }
 
     companion object {
@@ -53,12 +55,12 @@ class ActivityList : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ActivityList.
+         * @return A new instance of fragment CreateActivity.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ActivityList().apply {
+            CreateActivity().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
