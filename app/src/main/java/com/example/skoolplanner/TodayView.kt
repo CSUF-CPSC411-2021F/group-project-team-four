fcf2b02a55a830044b1ef2e2dcf9a70dfb819fe8
cspprice <cspprice@gmail.com>
2021-10-21 14:02:21 -0700

    Updated TodayView to correctly retrieve values from the calendar to be used when displaying activities on the main page and correctly retrieve values from the spinner to be used when sorting activities.

--- app/src/main/java/com/example/skoolplanner/TodayView.kt
+++ app/src/main/java/com/example/skoolplanner/TodayView.kt
@@ -2,18 +2,28 @@ package com.example.skoolplanner
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
+import android.widget.*
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.databinding.ViewDataBinding
+import java.text.DateFormat
+import java.util.*
 
 
 class TodayView : Fragment() {
+
+    val sortPriority: String = "Priority"
+    val sortDate: String = "Date"
+    val sortAlpha: String = "A-Z"
+    val sortRevAlpha: String = "Z-A"
+
+
+    var clickedDate: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +42,32 @@ class TodayView : Fragment() {
             spinner.adapter = adapter
         }
 
+        // Call sort function when spinner item is selected
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                sortActivities(parent.getItemAtPosition(position).toString())
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        val calendar = Calendar.getInstance()
+        val calendarView: CalendarView = binding.root.findViewById(R.id.simpleCalendarView)
+        val dateFormatter = DateFormat.getDateInstance(DateFormat.SHORT)
+        clickedDate = dateFormatter.format(Date())
+        Log.d("DebugLog", "Starting Date:")
+        Log.d("DebugLog",clickedDate)
+
+        calendarView.setOnDateChangeListener { view, year, month, day ->
+            calendar.set(year,month,day)
+
+            clickedDate = dateFormatter.format(calendar.time)
+            Log.d("DebugLog", "Clicked Date:")
+            Log.d("DebugLog",clickedDate)
+        }
+
+        
         return binding.root
     }
 
@@ -47,8 +83,12 @@ class TodayView : Fragment() {
     /*
         This function will be called when the user sorts the activities
     */
-    fun sortActivities(){
-
+    fun sortActivities(sortType: String){
+        Log.d("DebugLog", "Clicked Spinner option:")
+        Log.d("DebugLog", sortType)
+        when (sortType) {
+            //TODO: finish 'when' block when sort companion is complete
+        }
     }
 
 }
